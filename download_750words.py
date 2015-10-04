ae9ff8fd246eff3b5de848f64f163c3c465ed9c2
Kent Inverarity <kinverarity@hotmail.com>
2015-10-04 16:42:05 +1030

    Bugfix - not downloading all.

--- download_750words.py
+++ download_750words.py
@@ -9,16 +9,22 @@ import m750
 def main():
     parser = get_cmdline_parser()
     args = parser.parse_args(sys.argv[1:])
-    text, entries = m750.download_750words(current=~args.all, download=args.path)
+    if args.all:
+        dl_current = False
+    else:
+        dl_current = True
+    text, entries = m750.download_750words(email=args.username, password=args.password, current=dl_current, download=args.path)
     
     
 def get_cmdline_parser():
     home = os.path.expanduser('~')
     parser = argparse.ArgumentParser('download 750words writing')
-    parser.add_argument('-p', '--path', default=os.path.join(home, 'Downloads'),
+    parser.add_argument('-P', '--path', default=os.path.join(home, 'Downloads'),
                         help='where to download to')
     parser.add_argument('-a', '--all', action='store_true',
                         help='download all months (default is only the current month)')
+    parser.add_argument("-u", "--username", default=None)
+    parser.add_argument("-p", "--password", default=None)
     return parser    
     
     
